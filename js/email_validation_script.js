17d99ae6fb88415cfc8bbd610490b41316cbd20c
AndreasV9 <andreasvassilakos@gmail.com>
2020-02-25 21:34:05 -0600

    Update email_validation_script.js

--- js/email_validation_script.js
+++ js/email_validation_script.js
@@ -1,6 +1,6 @@
 function ValidateEmail(email) 
 {
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(signup.email))
   {
     return (true)
   }
